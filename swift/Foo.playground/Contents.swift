2315eca935933c0a7c38b4e687e320d82f109ec6
Jonathan Rasmusson <rasmus4200@yahoo.com>
2018-07-11 07:02:35 -0600

    Clean up

--- swift/Foo.playground/Contents.swift
+++
@@ -1,7 +0,0 @@
-//: Playground - noun: a place where people can play
-
-import UIKit
-
-let optionalInt: Int? = 5
-let unwrappedInt: Int = optionalInt!
-
--- swift/TheBasics.playground/Contents.swift
+++
@@ -1,8 +0,0 @@
-//: The Basics
-
-import UIKit
-import Foundation
-
-// https://docs.swift.org/swift-book/LanguageGuide/TheBasics.html
-
-var str = "Hello, playground"
