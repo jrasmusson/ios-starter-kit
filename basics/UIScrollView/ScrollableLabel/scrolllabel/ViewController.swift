47dee31e8582f85d8527e0c74aef0ce695a37e73
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-07-30 13:33:33 -0600

    cleanup

---
+++ basics/UIScrollView/ScrollableLabel/scrolllabel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  scrolllabel
+//
+//  Created by jrasmusson on 2021-07-29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
