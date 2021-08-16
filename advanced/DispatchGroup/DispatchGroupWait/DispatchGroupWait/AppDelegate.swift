28a79cb00e4d92c5b5f2c5bcaa5a119137adc146
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-08-16 16:36:29 -0600

    cleanup

---
+++ advanced/DispatchGroup/DispatchGroupWait/DispatchGroupWait/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  DispatchGroupWait
+//
+//  Created by jrasmusson on 2021-08-16.
+//
+
+import UIKit
+
+@main
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+    
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
+        
+        window = UIWindow(frame: UIScreen.main.bounds)
+        window?.makeKeyAndVisible()
+        window?.backgroundColor = .systemBackground
+        window?.rootViewController = ViewController()
+        
+        return true
+    }
+
+}
+
