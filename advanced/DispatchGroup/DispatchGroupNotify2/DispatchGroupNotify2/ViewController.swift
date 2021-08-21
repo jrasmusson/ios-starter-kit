96663ad3160fa93755d3eb04965b3cb67298142e
Jonathan Rasmusson <rasmus4200@gmail.com>
2021-08-21 07:41:20 -0600

    cleanup

---
+++ advanced/DispatchGroup/DispatchGroupNotify2/DispatchGroupNotify2/ViewController.swift
@@ -0,0 +1,131 @@
+//
+//  ViewController.swift
+//  DispatchGroupNotify2
+//
+//  Created by jrasmusson on 2021-08-21.
+//
+
+import UIKit
+
+struct Profile: Codable {
+    let id: String
+    let name: String
+}
+
+struct Entitlement: Codable {
+    let id: String
+    let access: String
+}
+
+struct Preference: Codable {
+    let id: String
+    let vehicle: String
+}
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var profileLabel: UILabel!
+    @IBOutlet var entitlementLabel: UILabel!
+    @IBOutlet var preferenceLabel: UILabel!
+    
+    @IBOutlet var loginButton: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
+
+
+// MARK: DispatchGroup
+extension ViewController {
+    private func fetchData() {
+        let group = DispatchGroup()
+        
+        group.enter()
+        fetchProfile { optionalProfile in
+            guard let profile = optionalProfile else { return }
+            self.profileLabel.text = profile.name
+            group.leave()
+        }
+
+        group.enter()
+        fetchEntitlement { optionalEntitlement in
+            guard let entitlement = optionalEntitlement else { return }
+            self.entitlementLabel.text = entitlement.access
+            group.leave()
+        }
+
+        group.enter()
+        fetchPreference { optionalEntitlement in
+            guard let entitlement = optionalEntitlement else { return }
+            self.preferenceLabel.text = entitlement.vehicle
+            group.leave()
+        }
+
+        group.notify(queue: .main) {
+            self.loginButton.isEnabled = true
+        }
+    }
+}
+
+// MARK: Networking
+extension ViewController {
+    
+    func fetchProfile(_ completion: @escaping (Profile?) -> Void) {
+        let url = URL(string: "https://fierce-retreat-36855.herokuapp.com/profile/1")!
+        URLSession.shared.dataTask(with: url) { (data, res, err) in
+            guard err == nil, let data = data else { return }
+            do {
+                let profile = try JSONDecoder().decode(Profile.self, from: data)
+                DispatchQueue.main.async {
+                    completion(profile)
+                }
+            } catch let err {
+                print(err)
+            }
+        }.resume()
+    }
+
+    func fetchEntitlement(_ completion: @escaping (Entitlement?) -> Void) {
+        let url = URL(string: "https://fierce-retreat-36855.herokuapp.com/entitlement/1")!
+        URLSession.shared.dataTask(with: url) { (data, res, err) in
+            guard err == nil, let data = data else { return }
+            do {
+                let entitlement = try JSONDecoder().decode(Entitlement.self, from: data)
+                DispatchQueue.main.async {
+                    completion(entitlement)
+                }
+            } catch let err {
+                print(err)
+            }
+        }.resume()
+    }
+
+    func fetchPreference(_ completion: @escaping (Preference?) -> Void) {
+        let url = URL(string: "https://fierce-retreat-36855.herokuapp.com/preference/1")!
+        URLSession.shared.dataTask(with: url) { (data, res, err) in
+            guard err == nil, let data = data else { return }
+            do {
+                let preference = try JSONDecoder().decode(Preference.self, from: data)
+                DispatchQueue.main.async {
+                    completion(preference)
+                }
+            } catch let err {
+                print(err)
+            }
+        }.resume()
+    }
+}
+
+// MARK: Actions
+extension ViewController {
+    @IBAction func goTapped(_ sender: Any) {
+        fetchData()
+    }
+    @IBAction func resetTapped(_ sender: Any) {
+        profileLabel.text = ""
+        entitlementLabel.text = ""
+        preferenceLabel.text = ""
+        loginButton.isEnabled = false
+    }
+}
--- advanced/DispatchGroup/Script.md
+++ advanced/DispatchGroup/Script.md
@@ -1,7 +1,54 @@
-# Script
+# DispatchGroup
 
-- start sinatra
-- 
+Handy class for grouping related calls together and not proceeding until they complete.
+
+Two typical use cases are:
+
+- Notify - how to be notified when a group of network calls complete
+- Wait - how to wait for a group of actions to complete before proceeding
+
+## Notify
+
+![](images/notify.png)
+
+```swift
+// MARK: DispatchGroup
+extension ViewController {
+    private func fetchData() {
+        let group = DispatchGroup()
+        
+        group.enter()
+        fetchProfile { optionalProfile in
+            guard let profile = optionalProfile else { return }
+            self.profileLabel.text = profile.name
+            group.leave()
+        }
+
+        group.enter()
+        fetchEntitlement { optionalEntitlement in
+            guard let entitlement = optionalEntitlement else { return }
+            self.entitlementLabel.text = entitlement.access
+            group.leave()
+        }
+
+        group.enter()
+        fetchPreference { optionalEntitlement in
+            guard let entitlement = optionalEntitlement else { return }
+            self.preferenceLabel.text = entitlement.vehicle
+            group.leave()
+        }
+
+        group.notify(queue: .main) {
+            self.loginButton.isEnabled = true
+        }
+    }
+}
+```
+
+## Wait
+
+
+
 
 ### Links that help
 
